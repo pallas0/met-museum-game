666ee870a18e06830b306582fa0fed0acfb88c98
Mike Starr <imhs@protonmail.com>
2022-01-24 16:23:29 -0800

    Removed redundant code in newRound function

--- index.js
+++ index.js
@@ -7,30 +7,29 @@
 function newRound() {
   // random num generator
   const apiObjectNum = Math.floor(Math.random() * 470000);
-  console.log(apiObjectNum);
+
   fetch(
     `https://collectionapi.metmuseum.org/public/collection/v1/objects/${apiObjectNum}`
   )
     .then((res) => res.json())
     .then((data) => {
       if (data.message === "ObjectID not found") {
-        console.log(data.message);
-        console.log(`running newRound() again`);
+        console.log("object not found in API. Re-running function");
+        newRound();
+      } else if (
+        data.primaryImage === "" ||
+        data.artistDisplayName === "" ||
+        data.accessionYear === "" ||
+        data.title === ""
+      ) {
+        //   console.log("Missing data. Running newRound() again");
         newRound();
       } else {
-        console.log(data);
-        if (
-          data.primaryImage === "" ||
-          data.artistDisplayName === "" ||
-          data.accessionYear === ""
-        ) {
-          console.log("Missing data. Running newRound() again");
-          newRound();
-        } else {
-          console.log(data.primaryImage);
-          console.log(data.artistDisplayName);
-          console.log(data.accessionYear);
-        }
+        console.log(apiObjectNum);
+        console.log("image url: ", data.primaryImage);
+        console.log("artist name: ", data.artistDisplayName);
+        console.log("year: ", data.accessionYear);
+        console.log("title: ", data.title);
       }
     });
 }
