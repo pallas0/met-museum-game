d69863af42102fb9d94353bf48f28c9531aa0f17
Mike Starr <imhs@protonmail.com>
2022-01-25 16:20:03 -0800

    saving changes before pulling main branch

--- index.js
+++ index.js
@@ -8,25 +8,28 @@ const option3 = document.querySelector("#objectID-3");
 const score = document.querySelector("#scorekeeper");
 const timeLeft = document.querySelector("#timeleft");
 
-//option1 = win, option2 and 3 = loss
-// let gameOption1;
-// let gameOption2;
-// let gameOption3;
-
 const gameChoicesObj = {
-  gameOption1: { objectId: "" },
-  gameOption2: { objectId: "" },
-  gameOption3: { objectId: "" },
+  gameOption1: {
+    objectId: "123",
+    artist: "picasso",
+    src: "google.com",
+    winner: "false",
+  },
+  gameOption2: {
+    objectId: "456",
+    artist: "monet",
+    src: "yahoo.com",
+    winner: "false",
+  },
+  gameOption3: {
+    objectId: "789",
+    artist: "michaelangelo",
+    src: "msn.com",
+    winner: "false",
+  },
 };
-// console.log(gameChoicesObj);
-console.log(gameChoicesObj);
-console.log(gameChoicesObj.gameOption1);
-console.log(gameChoicesObj.gameOption1["objectId"]);
 
 function newRound() {
-  // random number generator
-  //   let randomNum = Math.floor(Math.random() * 494);
-
   fetch(
     `https://collectionapi.metmuseum.org/public/collection/v1/search?q=cat&hasImages=true`
     // ${apiObjectNum}`
@@ -42,54 +45,37 @@ function newRound() {
       console.log(gameChoicesObj);
     });
 
-  /*
-    const gameChoicesObj = {
-  gameOption1: { objectId: "" },
-  gameOption2: { objectId: "" },
-  gameOption3: { objectId: "" },
-};
- */
-
-  /*
-  1. run randomNum generator 1-3
-  2. pass that number into gameChoicesObj[`gameOption${num}`], to populate DOM
-  3. For correct choice, pass in ".winner" class attribute into <li>
-  4. run if else statement, if other numbers are != randomNum, then only use that to fill in options2 and 3
-  */
   const indexCorrect = Math.floor(Math.random() * (4 - 1) + 1);
 
-  console.log(gameChoicesObj.gameOption1.objectId);
+  let gameOptionRand = gameChoicesObj[`gameOption${indexCorrect}`].objectId;
 
   fetch(
-    `https://collectionapi.metmuseum.org/public/collection/v1/objects/${
-      gameChoicesObj[`gameOption${indexCorrect}`].objectId
-    }`
+    `https://collectionapi.metmuseum.org/public/collection/v1/objects/${gameOptionRand}`
   )
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
 
-      //   if (data.message === "ObjectID not found") {
-      //     console.log("object not found in API. Re-running function");
-      //     newRound();
-      //   } else if (
-      //     data.primaryImageSmall === "" ||
-      //     data.artistDisplayName === "" ||
-      //     data.accessionYear === "" ||
-      //     data.title === ""
-      //   ) {
-      //     console.log("Query failed. Running newRound() again");
-      //     newRound();
-      //   } else {
-      //     console.log(apiObjectNum);
-      //     console.log("image url: ", data.primaryImageSmall);
-      //     console.log("artist name: ", data.artistDisplayName);
-      //     console.log("year: ", data.accessionYear);
-      //     console.log("title: ", data.title);
+      if (data.message === "ObjectID not found") {
+        console.log("object not found in API. Re-running function");
+        newRound();
+      } else if (
+        data.primaryImageSmall === "" ||
+        data.artistDisplayName === "" ||
+        data.accessionYear === "" ||
+        data.title === ""
+      ) {
+        console.log("Query failed. Running newRound() again");
+        newRound();
+      } else {
+        console.log("image url: ", data.primaryImageSmall);
+        console.log("artist name: ", data.artistDisplayName);
+        console.log("year: ", data.accessionYear);
+        console.log("title: ", data.title);
 
-      //     mainImage.src = data.primaryImageSmall;
-      //     mainImage.style.width = "350px";
-      //   }
+        mainImage.src = data.primaryImageSmall;
+        mainImage.style.width = "350px";
+      }
     });
 }
 
