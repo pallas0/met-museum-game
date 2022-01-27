20a91e79f3e184740df104b0a17df0442b0af1fa
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-01-26 19:29:24 -0800

    fixed async loading issue with first fetch call

--- game.js
+++ game.js
@@ -1,3 +1,9 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    welcomeScreen()
+    init()    
+});
+//document selectors
 const mainImage = document.getElementById("main-image");
 const option1 = document.getElementById("objectID-1");
 const option2 = document.getElementById("objectID-2");
@@ -7,18 +13,23 @@ const scoreEl = document.getElementById("scorekeeper");
 const timeLeftEl = document.getElementById("timeleft");
 const welcomePopupEl = document.getElementById("modal-holder")
 
-const newGameButton = document.querySelector('.new-game-button')
-newGameButton.addEventListener("click", function(e) {
-    //e.preventDefault()
-    console.log("clickity click")
-    window.location.reload()
-})
-
 const totalCorrectScore = document.querySelector("#total-correct");
 const totalQuestionsScore = document.querySelector("#total-questions");
 
+const newGameButton = document.querySelector('.new-game-button')
+newGameButton.addEventListener("click", function(e) {
+    //e.preventDefault()
+    window.location.reload()
+    reset();
+})
+
+
 let correctAnswers = 0;
 let totalQuestions = 0;
+
+function init() {
+    goSearch();
+}
 
 const baseSearchParam =
   "https://collectionapi.metmuseum.org/public/collection/v1/search?medium=Paintings&q=cat&department=13";
@@ -40,25 +51,25 @@ function goSearch(searchTerm) {
   //then the artist value should be filled in by a second fetch call to each of the relevant objects
   const gameOptions = {
       "option1": {
-        objectID: 01,
-        artist: "a",
-        title: "title",
-        year: 0000,
-        image: "url",
+        "objectID": 01,
+        "artist": "a",
+        "title": "title",
+        "year": 0000,
+        "image": "url",
       },
         "option2": {
-          objectID: 02,
-          artist: "b",
-          title: "title",
-          year: 0000,
-          image: "url",
+          "objectID": 02,
+          "artist": "b",
+          "title": "title",
+          "year": 0000,
+          "image": "url",
         },
         "option3": {
-          objectID: 03,
-          artist: "c",
-          title: "title",
-          year: 0000,
-          image: "url",
+          "objectID": 03,
+          "artist": "c",
+          "title": "title",
+          "year": 0000,
+          "image": "url",
         }
     }
     
@@ -69,111 +80,32 @@ function goSearch(searchTerm) {
 //then the artist value should be filled in by a second fetch call to each of the relevant objects
 
 
-function searchResults(data) {
+async function searchResults(data) {
   //this fills in our game pieces with randomly chosen objects
   for (let option in gameOptions) {
-    // debugger
-    gameOptions[option].objectID = data.objectIDs[randomNum(1, 500)];
-    let objectToFind = gameOptions[option].objectID;
-    //use our three objectIDs to then search for each object and gather data
-    fetch(
-      `https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectToFind}`
-    )
-      .then((resp) => resp.json())
-      //next we set the artist name in our local object based on the info from the api
-      .then((data) => {
-        // this points to the gameOptions object, and for each option.artist in the object
-        //it will set that param to data.artistDisplayName as it comes from the api
-        // -->possible issue here is what if there is no displayName attribute for the object in the api?
-        gameOptions[option].artist = data.artistDisplayName;
-        gameOptions[option].image = data.primaryImageSmall;
-        gameOptions[option].title = data.title;
-        gameOptions[option].year = data.objectEndDate;
-      });
+        gameOptions[option].objectID = data.objectIDs[randomNum(1, 500)];
+        let objectToFind = gameOptions[option].objectID;
+        //use our three objectIDs to then search for each object and gather data
+        await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectToFind}`)
+        .then((resp) => resp.json())
+        //next we set the artist name in our local object based on the info from the api
+        .then((data) => {
+            // this points to the gameOptions object, and for each option.artist in the object
+            //it will set that param to data.artistDisplayName as it comes from the api 
+            gameOptions[option].artist = data.artistDisplayName;
+            gameOptions[option].image = data.primaryImageSmall;
+            gameOptions[option].title = data.title;
+            gameOptions[option].year = data.objectEndDate;
+        });
+    }
+  initialLoad()
   }
-}
-
-
-
-function reverseLookUp(objectIdToLookUp) {
-  fetch(
-    `https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectIdToLookUp}`
-  )
-    .then((res) => res.json())
-    .then(function (data) {
-    })
-}
-
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-        welcomeScreen()
-    // welcomePopupEl.toggle()
-   
-});
 function welcomeScreen() {
     var myModal = new bootstrap.Modal(document.getElementById('welcome-popup'), {
         keyboard: false
       })
       myModal.show();
 }
-
-
- 
-// const mockPieces = {
-//   option1: {
-//     objectID: 12345,
-//     artist: "Tulip Painter",
-//     title: "A painting of tulips",
-//     year: 1964,
-//     image: "https://dummyimage.com/600x400/000/fff&text=A+painting+of+tulips",
-//   },
-//   option2: {
-//     objectID: 6789,
-//     artist: "Ocean Master",
-//     year: 1924,
-//     title: "The beautiful ocean mists warm my fate with baseballs",
-//     image:
-//       "https://dummyimage.com/600x400/000/0011ff&text=A+painting+of+the+ocean",
-//   },
-//   option3: {
-//     objectID: 101112,
-//     artist: "Humanist Painter",
-//     year: 1789,
-//     title: "A painting of the fall of man",
-//     image:
-//       "https://dummyimage.com/600x400/000/ff00fb&text=The+fall+of+man+as+illustrated+by+a+dead+white+guy",
-//   },
-//   correct: {
-//     objectID: "66666",
-//     artist: "Correct Answer Painter",
-//     year: 9999,
-//     title: "A painting of the correct answer",
-//     image:
-//       "https://previews.123rf.com/images/dirkercken/dirkercken1312/dirkercken131200024/24419932-correct-answer-right-choice.jpg",
-//   },
-// };
-
-function initialLoad() {
-  goSearch();
-  // pick a random dom element to make the correct answer
-  let correctAnswerElement = `option${randomNum(1, 4)}`;
-  //create a variable to store the correct answer object
-  randomizeWinnerOptions();
-  //set the id of each of the board pieces to match the input objectid
-  option1.setAttribute("data-id", gameOptions.option1.objectID);
-  option2.setAttribute("data-id", gameOptions.option2.objectID);
-  option3.setAttribute("data-id", gameOptions.option3.objectID);
-  mainImage.setAttribute("data-id", gameOptions.correct.objectID);
-
-  mainImage.src = gameOptions.correct.image;
-  option1.textContent = gameOptions.option1.artist;
-  option2.textContent = gameOptions.option2.artist;
-  option3.textContent = gameOptions.option3.artist;
-}
-
-initialLoad();
-
 function randomizeWinnerOptions() {
   const randNum = Math.floor(Math.random() * (4 - 1) + 1);
   console.log(randNum);
@@ -183,6 +115,24 @@ function randomizeWinnerOptions() {
     }
   }
 }
+function initialLoad() {
+  //set the id of each of the board pieces to match the input objectid
+  option1.setAttribute("data-id", gameOptions.option1.objectID);
+  option2.setAttribute("data-id", gameOptions.option2.objectID);
+  option3.setAttribute("data-id", gameOptions.option3.objectID);
+  //set the text on the game pieces
+  option1.textContent = gameOptions.option1.artist;
+  option2.textContent = gameOptions.option2.artist;
+  option3.textContent = gameOptions.option3.artist;
+  //below randomly chooses from the three existing options to choose a winner and make a new obj
+  randomizeWinnerOptions();
+  //the below attribute won't work if we don't fire randomizeWinnerOptions first
+  mainImage.setAttribute("data-id", gameOptions.correct.objectID);
+  mainImage.src = gameOptions.correct.image;
+}
+
+
+
 
 function correctAnnouncment() {
   //select our modal dom elements
