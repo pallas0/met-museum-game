b286236834e1d8f41f94020b4571af00d60ae805
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-01-26 23:08:15 -0800

    hooking the genny to the html

--- quote-generator.js
+++ quote-generator.js
@@ -1,19 +1,26 @@
 //this will simply be a data array of quotes - they are randomyl selected and then injected into the HTML in place - if this script gets bazongled it's all good!
 
+let injectpoint = document.getElementById('cheekynote')
+
 const quotes = [
     "'Every artist was first an amateur' – Ralph Waldo Emerson",
     "'Creativity takes courage' – Henri Matisse",
     "'You don't take a photograph, you make it' – Ansel Adams",
     "'We don't make mistakes, just happy little accidents.' – Bob Ross",
     "'Creativity is contagious, pass it on' – Albert Einstein",
-    
+    "'A line is a dot that went for a walk.' – Paul Klee",
+    "'The job of the artist is always to deepen the mystery' -Francis Bacon",
+    "Lesser artists borrow, great artists steal.' -Igot Stravinsky",
+    "'Art is the most beautiful of all lies.' -Claude Debussy"
 
 
 ]
 
 let randomNum = Math.floor(Math.random() * arry.length);
 
-function gimmeQuote(arry){
+function gimme(arry){
     return arry[randomNum]
 }
-gimmeQuote(arry)
+//gimme(quotes)
+
+injectpoint.innerText = gimme(quotes)
