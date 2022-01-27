b32ed8149faa0b155e0000d9d31d578779ed0ff9
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-01-26 23:16:02 -0800

    quote genny works

--- quote-generator.js
+++ quote-generator.js
@@ -3,23 +3,26 @@
 let injectpoint = document.getElementById('cheekynote')
 
 const quotes = [
-    "'Every artist was first an amateur' – Ralph Waldo Emerson",
-    "'Creativity takes courage' – Henri Matisse",
-    "'You don't take a photograph, you make it' – Ansel Adams",
+    "'Every artist was first an amateur.' – Ralph Waldo Emerson",
+    "'Creativity takes courage.' – Henri Matisse",
+    "'You don't take a photograph, you make it.' – Ansel Adams",
     "'We don't make mistakes, just happy little accidents.' – Bob Ross",
-    "'Creativity is contagious, pass it on' – Albert Einstein",
+    "'Creativity is contagious, pass it on.' – Albert Einstein",
     "'A line is a dot that went for a walk.' – Paul Klee",
-    "'The job of the artist is always to deepen the mystery' -Francis Bacon",
-    "Lesser artists borrow, great artists steal.' -Igot Stravinsky",
-    "'Art is the most beautiful of all lies.' -Claude Debussy"
-
-
+    "'The job of the artist is always to deepen the mystery.' -Francis Bacon",
+    "'Lesser artists borrow, great artists steal.' -Igot Stravinsky",
+    "'Art is the most beautiful of all lies.' -Claude Debussy",
+    "Did you know that the Met will turn 122 years old this year?",
+    "The Metropolitan Museum presents more exhibitions than any art museum in the world.",
+    "Founded in 1870, the Met collection spans 5000 years of culture.",
+    "The Met collection contains more than a million objects!",
+    "The Met is located on 5th Ave in NYC!"
 ]
 
-let randomNum = Math.floor(Math.random() * arry.length);
+let quoteRando = Math.floor(Math.random() * quotes.length);
 
 function gimme(arry){
-    return arry[randomNum]
+    return arry[quoteRando]
 }
 //gimme(quotes)
 
