65da48cef98ea988fa5ac4e92e79c268a6672b1b
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-01-26 20:07:26 -0800

    event handlers fixed - popups loading and populating correctly

--- game.js
+++ game.js
@@ -13,17 +13,18 @@ window.addEventListener('DOMContentLoaded', (event) => {
   const timeLeftEl = document.getElementById("timeleft");
   const welcomePopupEl = document.getElementById("modal-holder")
   
-  const totalCorrectScore = document.querySelector("#total-correct");
-  const totalQuestionsScore = document.querySelector("#total-questions");
+  const totalCorrectScore = document.getElementById("total-correct");
+  const totalQuestionsScore = document.getElementById("total-questions");
   
   const newGameButton = document.querySelector('.new-game-button')
  //event listeners 
   option1.addEventListener("click", winLogic);
   option2.addEventListener("click", winLogic);
   option3.addEventListener("click", winLogic);
+
   newGameButton.addEventListener("click", function(e) {
-    //e.preventDefault()
-    window.location.reload()
+    // //e.preventDefault()
+    // window.location.reload()
     reset();
 })
 
@@ -104,81 +105,91 @@ async function searchResults(data) {
     }
   initialLoad()
   }
-function welcomeScreen() {
-    var myModal = new bootstrap.Modal(document.getElementById('welcome-popup'), {
-        keyboard: false
-      })
-      myModal.show();
-}
-function randomizeWinnerOptions() {
-  const randNum = Math.floor(Math.random() * (4 - 1) + 1);
-  console.log(randNum);
-  for (let option in gameOptions) {
-    if (option === `option${randNum}`) {
-      gameOptions["correct"] = gameOptions[option];
+  function randomizeWinnerOptions() {
+    const randNum = Math.floor(Math.random() * (4 - 1) + 1);
+    // console.log(randNum);
+    for (let option in gameOptions) {
+      if (option === `option${randNum}`) {
+        gameOptions["correct"] = gameOptions[option];
+      }
     }
   }
-}
-function initialLoad() {
-  //set the id of each of the board pieces to match the input objectid
-  option1.setAttribute("data-id", gameOptions.option1.objectID);
-  option2.setAttribute("data-id", gameOptions.option2.objectID);
-  option3.setAttribute("data-id", gameOptions.option3.objectID);
-  //set the text on the game pieces
-  option1.textContent = gameOptions.option1.artist;
-  option2.textContent = gameOptions.option2.artist;
-  option3.textContent = gameOptions.option3.artist;
-  //below randomly chooses from the three existing options to choose a winner and make a new obj
-  randomizeWinnerOptions();
-  //the below attribute won't work if we don't fire randomizeWinnerOptions first
-  mainImage.setAttribute("data-id", gameOptions.correct.objectID);
-  mainImage.src = gameOptions.correct.image;
-}
-
-
-
-//this function fills in the information for our popup with the correct info
-function correctAnnouncment() {
-  //select our modal dom elements
-  const modalObjectTitle = document.getElementById("modal-object-title");
-  const modalObjectArtist = document.getElementById("modal-object-artist");
-  const modalObjectArtistInsert = document.getElementById("artist-insert");
-  const modalObjectYear = document.getElementById("modal-object-year");
-  const modalObjectImage = document.getElementById("modal-object-image");
-  //replace elements with our correct answer
-  modalObjectTitle.textContent = gameOptions.correct.title;
-  modalObjectArtist.textContent = gameOptions.correct.artist;
-  modalObjectArtistInsert.textContent = gameOptions.correct.artist;
-  modalObjectYear.textContent = gameOptions.correct.year;
-  modalObjectImage.src = gameOptions.correct.image;
-}
-
-// winner logic- add to event listeners on option buttons
-function winLogic() {
-  //see if data-id matches mainImage.dat-id
-  if (this.getAttribute("data-id") === mainImage.getAttribute("data-id")) {
-    alert("correct! you know your artists!!!");
-    console.log("correct!");
-    correctAnswers++;
-    totalQuestions++;
-    totalCorrectScore.textContent = correctAnswers;
-    totalQuestionsScore.textContent = totalQuestions;
-    // openModalWindow()
-    // reset();
-  } else {
-    alert("Incorrect! Better luck next round!");
-    console.log("wrong!");
-    totalQuestions++;
-    totalQuestionsScore.textContent = totalQuestions;
-    // openModalWindow()
-    // reset();
+  function initialLoad() {
+    //set the id of each of the board pieces to match the input objectid
+    option1.setAttribute("data-id", gameOptions.option1.objectID);
+    option2.setAttribute("data-id", gameOptions.option2.objectID);
+    option3.setAttribute("data-id", gameOptions.option3.objectID);
+    //set the text on the game pieces
+    option1.textContent = gameOptions.option1.artist;
+    option2.textContent = gameOptions.option2.artist;
+    option3.textContent = gameOptions.option3.artist;
+    //below randomly chooses from the three existing options to choose a winner and make a new obj
+    randomizeWinnerOptions();
+    //the below attribute won't work if we don't fire randomizeWinnerOptions first
+    mainImage.setAttribute("data-id", gameOptions.correct.objectID);
+    mainImage.src = gameOptions.correct.image;
   }
-}
-//reset the game board (currently just clears, will need to fire goSearch again)
-const reset = () => {
-  option1.setAttribute("data-id", "");
-  option2.setAttribute("data-id", "");
-  option3.setAttribute("data-id", "");
-  mainImage.setAttribute("data-id", "");
-};
-
+  
+  
+  
+  
+  // winner logic- add to event listeners on option buttons
+  function winLogic() {
+    //see if data-id matches mainImage.dat-id
+    if (this.getAttribute("data-id") === mainImage.getAttribute("data-id")) {
+      correctAnswerPopUp();
+      console.log("correct!");
+      correctAnswers++;
+      totalQuestions++;
+      totalCorrectScore.textContent = correctAnswers;
+      totalQuestionsScore.textContent = totalQuestions;
+      // openModalWindow()
+      // reset();
+    } else {
+      alert("Incorrect! Better luck next round!");
+      console.log("wrong!");
+      totalQuestions++;
+      totalQuestionsScore.textContent = totalQuestions;
+      // openModalWindow()
+      // reset();
+    }
+  }
+  //reset the game board (currently just clears, will need to fire goSearch again)
+  const reset = () => {
+    option1.setAttribute("data-id", "");
+    option2.setAttribute("data-id", "");
+    option3.setAttribute("data-id", "");
+    mainImage.setAttribute("data-id", "");
+    welcomeScreen()
+    goSearch()
+  };
+  
+  // popup handlers below
+  function welcomeScreen() {
+    const welcomePopUp = new bootstrap.Modal(document.getElementById('welcome-popup'), {
+      keyboard: false
+    })
+    welcomePopUp.show();
+  }
+  //this function fills in the information for our popup with the correct info and opens it
+  function correctAnswerPopUp() {
+    //select our modal dom elements
+    const modalObjectTitle = document.getElementById("modal-object-title");
+    const modalObjectArtist = document.getElementById("modal-object-artist");
+    const modalObjectArtistInsert = document.getElementById("artist-insert");
+    const modalObjectYear = document.getElementById("modal-object-year");
+    const modalObjectImage = document.getElementById("modal-object-image");
+    //replace elements with our correct answer
+    modalObjectTitle.textContent = gameOptions.correct.title;
+    modalObjectArtist.textContent = gameOptions.correct.artist;
+    modalObjectArtistInsert.textContent = gameOptions.correct.artist;
+    modalObjectYear.textContent = gameOptions.correct.year;
+    modalObjectImage.src = gameOptions.correct.image;
+    //this open the popup
+    const correctAnsPopUp = new bootstrap.Modal(document.getElementById('correct-popup'), {
+      keyboard: false
+    })
+    correctAnsPopUp.show();
+  }
+  
+  
