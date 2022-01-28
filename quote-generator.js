0dcbbceddfe1fa67351f7feadbcc490a42fe826b
amelia.risner0@gmail.com <arisner@berkeley.edu>
2022-01-27 16:06:59 -0800

    quote generator added

--- quote-generator.js
+++ quote-generator.js
@@ -2,27 +2,21 @@
 
 let injectpoint = document.getElementById('cheekynote')
 
-const quotes = [
-    "'Every artist was first an amateur.' – Ralph Waldo Emerson",
-    "'Creativity takes courage.' – Henri Matisse",
-    "'You don't take a photograph, you make it.' – Ansel Adams",
-    "'We don't make mistakes, just happy little accidents.' – Bob Ross",
-    "'Creativity is contagious, pass it on.' – Albert Einstein",
-    "'A line is a dot that went for a walk.' – Paul Klee",
-    "'The job of the artist is always to deepen the mystery.' -Francis Bacon",
-    "'Lesser artists borrow, great artists steal.' -Igot Stravinsky",
-    "'Art is the most beautiful of all lies.' -Claude Debussy",
-    "Did you know that the Met will turn 122 years old this year?",
-    "The Metropolitan Museum presents more exhibitions than any art museum in the world.",
-    "Founded in 1870, the Met collection spans 5000 years of culture.",
-    "The Met collection contains more than a million objects!",
-    "The Met is located on 5th Ave in NYC!"
-]
 
-let quoteRando = Math.floor(Math.random() * quotes.length);
+function quotePost() {
+    fetch("http://localhost:3000/quotes")
+    .then(response => response.json())
+    .then((data) => {
+            insertQuotes(data);
+        })
 
-function gimme(arry){
-    return arry[quoteRando]
+    }
+
+function insertQuotes(data) {
+    let quoteRando = Math.floor(Math.random() * data.length);
+    injectpoint.innerText = data[quoteRando]
 }
 
-injectpoint.innerText = gimme(quotes)
+quotePost()
+
+
