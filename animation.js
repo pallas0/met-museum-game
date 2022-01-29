f08123c54d20914890fc6eac0afd0d9e7b61dd7e
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-01-28 20:41:35 -0800

    first commit on new fork - already made a couple css fixes to make the footer stick

---
+++ animation.js
@@ -0,0 +1,9 @@
+let loadingEl = document.getElementById('loading')
+
+let changeState = document.getElementById('option1')
+
+if (changeState.textContent = "") {
+    loadingEl.style.display = "flex" 
+
+    }
+}
